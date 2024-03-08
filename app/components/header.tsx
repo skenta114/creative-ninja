a255ad8708266a872ba22cd61cb97971cb786805
fkoki0309 <fkoki0309@gmail.com>
2024-03-08 15:40:17 +0900

    ハンバーガーメニュー追加

---
+++ app/components/header.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import BurgerMenu from './burgerMenu'
+import Link from 'next/link'
+
+export default function Header () {
+  return (
+    <header className="w-full p-2 sticky bg-gray-100">
+      <nav className="p-2">
+        <div className="hidden md:flex justify-between  min-w-3xl">
+          <div className='opacity:0'></div>
+          <div className="ml-1 text-xl  font-fantasy">
+            <Link href={"/"}>アプリ名</Link>
+          </div>
+          <BurgerMenu />
+        </div>
+        <div className="md:hidden flex justify-between  max-w-3xl ">
+          <div className='opacity:0'></div>
+          <div className="text-xl font-fantasy">
+          <Link href={"/"}>アプリ名</Link>
+          </div>
+          <div className="text-xl justify-end font-bold">
+            <BurgerMenu />
+          </div>
+        </div>
+      </nav>
+    </header>
+      
+    
+  )
+}
+
+
+
