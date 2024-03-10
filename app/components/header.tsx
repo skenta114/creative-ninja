4027ed5807c6c853553c1cf608827475fe1e580e
fkoki0309 <fkoki0309@gmail.com>
2024-03-11 04:13:25 +0900

    UIの修正

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import BurgerMenu from './burgerMenu'
 import Link from 'next/link'
-
+import Image from "next/image";
+import localImage from "../../public/Icon.jpg";
 export default function Header() {
+
   return (
-    <header className="w-full p-2 sticky bg-[#BDE3FF]">
+    <header className="w-full p-2 sticky bg-white ">
       <nav className="p-2">
         <div className="hidden md:flex justify-between  min-w-3xl">
           <div className='opacity:0'></div>
-          <div className="ml-1 text-xl  font-fantasy">
-            <Link href={"/"}>アプリ名</Link>
+          <div className="ml-1 text-xl">
+            <Link href={"/"}><div className="flex flex-col min-h-0 md:flex-row">
+              <Image
+                src={localImage}
+                alt="Be.stie"
+                sizes="(max-width: 768px) 15vw, 15vw"
+                className="flex-1 w-full h-auto object-cover"
+              />
+
+
+            </div></Link>
           </div>
           <BurgerMenu />
         </div>
         <div className="md:hidden flex justify-between  max-w-3xl ">
           <div className='opacity:0'></div>
-          <div className="text-xl font-fantasy">
-            <Link href={"/"}>アプリ名</Link>
+          <div className="text-xl">
+            <Link href={"/"} ><div className="flex flex-col min-h-0 md:flex-row">
+              <Image
+                src={localImage}
+                alt="Be.stie"
+                sizes="(max-width: 768px) 15vw, 15vw"
+                className="flex-1 w-full h-auto object-cover"
+              />
+
+
+            </div></Link>
           </div>
           <div className="text-xl justify-end font-bold">
             <BurgerMenu />
